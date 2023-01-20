fcb7ef2058a47ce77d8f4ab1b115d6676f4cb410
Lorran de Barros <lorranbarrinha@gmail.com>
2023-01-20 08:12:54 -0300

    Major movement and jumping fixes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variables for player movement 
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     public float doubleJumpForce = 2.5f;
@@ -12,49 +11,58 @@ public class PlayerController : MonoBehaviour
     public bool canDoubleJump = false;
 
     private Rigidbody2D rb;
-    public bool isJumping = false;
-    private bool isOnRoof = false;
+    private bool isJumping = false;
+    public bool isOnRoof = false;
 
-    public float gravityChangeSpeed = 1f; //Speed to change the gravity
-    public Animator _camAnimator; //Animator of camera gameobj
-    public Animator playerAnimator;
-
+    public float _gravityChangeSpeed = 6f;
+    public Animator _camAnimator;
+    Animator thisAnim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Physics2D.gravity = new Vector2(0f, -9.8f * gravityChangeSpeed);
-        isOnRoof = true;
+        Physics2D.gravity = new Vector2(0f, -9.8f * _gravityChangeSpeed);
+        isOnRoof = false;
+        thisAnim = GetComponent<Animator>();
     }
 
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
+
+        thisAnim.SetBool("Jumping", isJumping);
+
+        if (isOnRoof)
+        {
+            moveX = moveX * -1;
+        }
         rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
-
-        playerAnimator.SetBool("Jumping", isJumping);
-
-        if(moveX != 0)
-        {
-            playerAnimator.SetBool("Walking", true);
-        }
-        else
-        {
-            playerAnimator.SetBool("Walking", false);
-        }
-
 
         // Check if the jump button is pressed
         if (Input.GetButtonDown("Jump"))
         {
             if (!isJumping)
             {
-                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                if (isOnRoof)
+                {
+                    rb.AddForce(new Vector2(0f, -jumpForce), ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                }
                 isJumping = true;
             }
             else if (canDoubleJump)
             {
-                rb.AddForce(new Vector2(0f, doubleJumpForce), ForceMode2D.Impulse);
+                if (isOnRoof)
+                {
+                    rb.AddForce(new Vector2(0f, -doubleJumpForce), ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(new Vector2(0f, doubleJumpForce), ForceMode2D.Impulse);
+                }
                 canDoubleJump = false;
             }
         }
@@ -65,30 +73,38 @@ public class PlayerController : MonoBehaviour
             if (!isOnRoof)
             {
                 // Change gravity and switch to roof
-                Physics2D.gravity = new Vector2(0f, -9.8f * gravityChangeSpeed);
+                Physics2D.gravity = new Vector2(0f, 9.8f * _gravityChangeSpeed);
+                transform.Rotate(180f, 0f, 0f);
                 isOnRoof = true;
-                _camAnimator.Play("ShiftFloor", -1, 0f);
+                _camAnimator.Play("ShiftRoof", -1, 0f);
             }
             else
             {
                 // Change gravity and switch to ground
-                Physics2D.gravity = new Vector2(0f, 9.8f * gravityChangeSpeed);
+                Physics2D.gravity = new Vector2(0f, -9.8f * _gravityChangeSpeed);
+                transform.Rotate(180f, 0f, 0f);
                 isOnRoof = false;
-                _camAnimator.Play("ShiftRoof", -1, 0f);
+                _camAnimator.Play("ShiftFloor", -1, 0f);
+
             }
         }
 
-
-
-
-        if(Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKeyDown(KeyCode.A) && !isOnRoof)
         {
-            this.transform.localScale = new Vector3(-1f, this.transform.localScale.y, this.transform.localScale.z);
+            this.transform.localScale = new Vector3(-1, this.transform.localScale.y, this.transform.localScale.z);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && !isOnRoof)
+        {
+            this.transform.localScale = new Vector3(1, this.transform.localScale.y, this.transform.localScale.z);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.A) && isOnRoof)
         {
-            this.transform.localScale = new Vector3(1f, this.transform.localScale.y, this.transform.localScale.z);
+            this.transform.localScale = new Vector3(1, this.transform.localScale.y, this.transform.localScale.z);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && isOnRoof)
+        {
+            this.transform.localScale = new Vector3(-1, this.transform.localScale.y, this.transform.localScale.z);
         }
     }
 
@@ -106,7 +122,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Function for taking damage
     public void TakeDamage(float damage)
     {
         health -= damage;
@@ -116,9 +131,8 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Function for player death
     private void Die()
     {
         // Code for death
     }
-    }
+}
